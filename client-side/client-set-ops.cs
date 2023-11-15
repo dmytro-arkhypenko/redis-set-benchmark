3eeddb46a571e44f494f67308fd1a114ce16b008
Dmytro Arkhypenko <DOA@simcorp.com>
2023-11-15 14:23:59 +0100

    Changed to BM.net

--- client-side/client-set-ops.cs
+++ client-side/client-set-ops.cs
@@ -1,25 +1,34 @@
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
 using MessagePack;
 using StackExchange.Redis;
 using System;
-using System.Diagnostics;
+using System.Collections.Generic;
 using System.Linq;
 
-class RedisBenchmark
+public class RedisBenchmark
 {
-    private readonly ConnectionMultiplexer redis;
-    private readonly IDatabase db;
+    private ConnectionMultiplexer redis;
+    private IDatabase db;
     private const int MaxValue = 100000;
     private const int SetSize = 50000;
     private const int TotalSets = 20;
-    private Random random = new Random();
+    private Random random = new Random(123456789);
 
-    public RedisBenchmark(string connectionString)
+    [Params(3, 5, 7, 10, 15, 20)]
+    public int NumberOfSets { get; set; }
+
+    [GlobalSetup]
+    public void Setup()
     {
-        redis = ConnectionMultiplexer.Connect(connectionString);
+        redis = ConnectionMultiplexer.Connect("localhost:6379");
         db = redis.GetDatabase();
+        PopulateSets();
     }
 
-    public void PopulateSets()
+    private void PopulateSets()
     {
         for (int i = 1; i <= TotalSets; i++)
         {
@@ -32,69 +41,64 @@ class RedisBenchmark
         }
     }
 
-    public HashSet<int> FetchSet(string key)
+    private HashSet<int> FetchSet(string key)
     {
         var byteValue = db.StringGet(key);
         return new HashSet<int>(MessagePackSerializer.Deserialize<int[]>(byteValue));
     }
 
-    public TimeSpan MeasureSetOperation(Func<RedisKey[], HashSet<int>> operation, RedisKey[] keys)
+    [Benchmark]
+    public HashSet<int> MeasureSetIntersection()
     {
-        var stopwatch = Stopwatch.StartNew();
-        operation(keys);
-        stopwatch.Stop();
-        return stopwatch.Elapsed;
+        var sets = Enumerable.Range(1, NumberOfSets).Select(i => (RedisKey)$"set{i}").ToArray();
+        var result = FetchSet(sets[0]);
+        for (int i = 1; i < sets.Length; i++)
+        {
+            var setN = FetchSet(sets[i]);
+            result.IntersectWith(setN);
+        }
+        return result;
     }
 
-    static void Main(string[] args)
+    [Benchmark]
+    public HashSet<int> MeasureSetUnion()
     {
-        var benchmark = new RedisBenchmark("localhost:6379");
-        benchmark.PopulateSets();
-
-        int[] setCounts = { 3, 5, 7, 10, 15, 20 };
-
-        foreach (var count in setCounts)
+        var sets = Enumerable.Range(1, NumberOfSets).Select(i => (RedisKey)$"set{i}").ToArray();
+        var result = FetchSet(sets[0]);
+        for (int i = 1; i < sets.Length; i++)
         {
-            // Select first 'count' sets for the operation
-            var keys = Enumerable.Range(1, count).Select(i => (RedisKey)$"set{i}").ToArray();
-
-            var intersectionTime = benchmark.MeasureSetOperation(sets =>
-            {
-                var result = benchmark.FetchSet(sets[0]);
-                for (int i = 1; i < sets.Length; i++)
-                {
-                    var setN = benchmark.FetchSet(sets[i]);
-                    result.IntersectWith(setN);
-                }
-                return result;
-            }, keys);
-            Console.WriteLine($"Intersection Time for {count} sets: {intersectionTime}");
-
-            var unionTime = benchmark.MeasureSetOperation(sets =>
-            {
-                var result = benchmark.FetchSet(sets[0]);
-                for (int i = 1; i < sets.Length; i++)
-                {
-                    var setN = benchmark.FetchSet(sets[i]);
-                    result.UnionWith(setN);
-                }
-                return result;
-            }, keys);
-            Console.WriteLine($"Union Time for {count} sets: {unionTime}");
-
-            var differenceTime = benchmark.MeasureSetOperation(sets =>
-            {
-                var result = benchmark.FetchSet(sets[0]);
-                for (int i = 1; i < sets.Length; i++)
-                {
-                    var setN = benchmark.FetchSet(sets[i]);
-                    result.ExceptWith(setN);
-                }
-                return result;
-            }, keys);
-            Console.WriteLine($"Difference Time for {count} sets: {differenceTime}");
-
-            Console.WriteLine();
+            var setN = FetchSet(sets[i]);
+            result.UnionWith(setN);
         }
+        return result;
+    }
+
+    [Benchmark]
+    public HashSet<int> MeasureSetDifference()
+    {
+        var sets = Enumerable.Range(1, NumberOfSets).Select(i => (RedisKey)$"set{i}").ToArray();
+        var result = FetchSet(sets[0]);
+        for (int i = 1; i < sets.Length; i++)
+        {
+            var setN = FetchSet(sets[i]);
+            result.ExceptWith(setN);
+        }
+        return result;
+    }
+
+
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = ManualConfig.Create(DefaultConfig.Instance)
+            .With(Job.Default
+                .WithWarmupCount(1)    // Number of warmup iterations
+                .WithIterationCount(3) // Number of target iterations
+            );
+
+        BenchmarkRunner.Run<RedisBenchmark>(config);
     }
 }
--- redis-side/redis-set-ops.cs
+++ redis-side/redis-set-ops.cs
@@ -1,65 +1,73 @@
 using StackExchange.Redis;
-using System;
-using System.Diagnostics;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
 using System.Linq;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
 
-class RedisBenchmark
+public class RedisBenchmark
 {
     private readonly ConnectionMultiplexer redis;
     private readonly IDatabase db;
     private const int MaxValue = 100000;
     private const int SetSize = 50000;
     private const int TotalSets = 20;
-    private Random random = new Random();
+    private Random random = new Random(123456789);
 
-    public RedisBenchmark(string connectionString)
+    [Params(3, 5, 7, 10, 15, 20)]
+    public int NumberOfSets { get; set; }
+
+    public RedisBenchmark()
     {
-        redis = ConnectionMultiplexer.Connect(connectionString);
+        redis = ConnectionMultiplexer.Connect("localhost:6379");
         db = redis.GetDatabase();
+        PopulateSets();
     }
 
-    public void PopulateSets()
+    private void PopulateSets()
     {
         for (int i = 1; i <= TotalSets; i++)
         {
             var key = $"set{i}";
             db.KeyDelete(key);
-
             RedisValue[] values = Enumerable.Range(0, SetSize).Select(_ => (RedisValue)(random.Next(1, MaxValue))).ToArray();
-
             db.SetAdd(key, values);
         }
     }
 
-    public TimeSpan MeasureSetOperation(Func<RedisValue[]> operation)
+    [Benchmark]
+    public void MeasureSetIntersection()
     {
-        var stopwatch = Stopwatch.StartNew();
-        operation();
-        stopwatch.Stop();
-        return stopwatch.Elapsed;
+        var keys = Enumerable.Range(1, NumberOfSets).Select(i => (RedisKey)$"set{i}").ToArray();
+        db.SetCombine(SetOperation.Intersect, keys);
     }
 
+    [Benchmark]
+    public void MeasureSetUnion()
+    {
+        var keys = Enumerable.Range(1, NumberOfSets).Select(i => (RedisKey)$"set{i}").ToArray();
+        db.SetCombine(SetOperation.Union, keys);
+    }
+
+    [Benchmark]
+    public void MeasureSetDifference()
+    {
+        var keys = Enumerable.Range(1, NumberOfSets).Select(i => (RedisKey)$"set{i}").ToArray();
+        db.SetCombine(SetOperation.Difference, keys);
+    }
+}
+
+class Program
+{
     static void Main(string[] args)
     {
-        var benchmark = new RedisBenchmark("localhost:6379");
-        benchmark.PopulateSets();
+        var config = ManualConfig.Create(DefaultConfig.Instance)
+            .With(Job.Default
+                .WithWarmupCount(1)    // Number of warmup iterations
+                .WithIterationCount(3) // Number of target iterations
+            );
 
-        // Measure for 3, 5, and 7 sets
-        int[] setCounts = { 3, 5, 7, 10, 15, 20 };
+        BenchmarkRunner.Run<RedisBenchmark>(config);
 
-        foreach (var count in setCounts)
-        {
-            // Select first 'count' sets for the operation
-            var keys = Enumerable.Range(1, count).Select(i => (RedisKey)$"set{i}").ToArray();
-
-            var intersectionTime = benchmark.MeasureSetOperation(() => benchmark.db.SetCombine(SetOperation.Intersect, keys));
-            Console.WriteLine($"Intersection Time for {count} sets: {intersectionTime}");
-
-            var unionTime = benchmark.MeasureSetOperation(() => benchmark.db.SetCombine(SetOperation.Union, keys));
-            Console.WriteLine($"Union Time for {count} sets: {unionTime}");
-
-            var differenceTime = benchmark.MeasureSetOperation(() => benchmark.db.SetCombine(SetOperation.Difference, keys));
-            Console.WriteLine($"Difference Time for {count} sets: {differenceTime}\n");
-        }
     }
 }
